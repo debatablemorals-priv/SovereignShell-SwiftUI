59b869ef265723212b05d00517b5fb8d21db7058
SovereignGenesis19 <c.w.carroll19@proton.me>
2026-03-08 13:41:45 +0000

    Route AIS event adapter through canonical event append path

--- Sources/SovereignShell-SwiftUI/Core/Security/AISEventLedgerAdapter.swift
+++ Sources/SovereignShell-SwiftUI/Core/Security/AISEventLedgerAdapter.swift
@@ -1,10 +1,7 @@
 import Foundation
 
 extension AISExecutionLedger {
-
     func appendEvent(_ event: AISEvent) throws {
-        let request = "AIS_EVENT:\(event.eventType.rawValue)"
-        let response = "AIS_STATE:\(event.trustState.rawValue)"
-        try append(request: request, response: response)
+        try append(event: event)
     }
 }
