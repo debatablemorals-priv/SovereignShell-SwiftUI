0f9ea8cddd29b5935a4019a45472ba33b8b43b69
SovereignGenesis19 <c.w.carroll19@proton.me>
2026-03-08 14:20:02 +0000

    Finalize application entrypoint and container injection

--- Sources/SovereignShell-SwiftUI/SovereignShellApp.swift
+++ Sources/SovereignShell-SwiftUI/SovereignShellApp.swift
@@ -2,9 +2,14 @@ import SwiftUI
 
 @main
 struct SovereignShellApp: App {
+
+    @StateObject
+    private var container = AppContainer()
+
     var body: some Scene {
         WindowGroup {
             RootView()
+                .environmentObject(container)
         }
     }
 }
